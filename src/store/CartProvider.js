211ba797748c81eae37ad833b0184a747e150bf6
NiyatiMPatel <patelnitie@gmail.com>
2022-05-22 12:49:36 +0530

    add-meals-updated

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,30 +8,71 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updatedTotalAmount =
-      state.totalAmount + action.item.price * action.item.amount;
+    let updatedTotalAmount;
 
-    const existingCartItemIndex = state.items.findIndex(
-      (item) => item.id === action.item.id
-    );
+    const existingCartItemIndex = state.items.findIndex((item) => {
+      return item.id === action.item.id;
+    });
+
     const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItem;
     let updatedItems;
 
     if (existingCartItem) {
-      const updatedItem = {
-        ...existingCartItem,
-        amount: existingCartItem.amount + action.item.amount,
-      };
+      if (existingCartItem.amount + action.item.amount > 5) {
+        updatedTotalAmount = state.totalAmount;
+        updatedItem = {
+          ...existingCartItem,
+          amount: 5,
+        };
+      } else {
+        updatedTotalAmount =
+          state.totalAmount + action.item.price * action.item.amount;
+        updatedItem = {
+          ...existingCartItem,
+          amount: existingCartItem.amount + action.item.amount,
+        };
+      }
+
       updatedItems = [...state.items];
       updatedItems[existingCartItemIndex] = updatedItem;
     } else {
+      updatedTotalAmount =
+        state.totalAmount + action.item.price * action.item.amount;
       updatedItems = state.items.concat(action.item);
     }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
     };
   }
+
+  // if (action.type === "ADD") {
+  //   const updatedTotalAmount =
+  //     state.totalAmount + action.item.price * action.item.amount;
+
+  //   const existingCartItemIndex = state.items.findIndex(
+  //     (item) => item.id === action.item.id
+  //   );
+  //   const existingCartItem = state.items[existingCartItemIndex];
+  //   let updatedItems;
+
+  //   if (existingCartItem) {
+  //     const updatedItem = {
+  //       ...existingCartItem,
+  //       amount: existingCartItem.amount + action.item.amount,
+  //     };
+  //     updatedItems = [...state.items];
+  //     updatedItems[existingCartItemIndex] = updatedItem;
+  //   } else {
+  //     updatedItems = state.items.concat(action.item);
+  //   }
+  //   return {
+  //     items: updatedItems,
+  //     totalAmount: updatedTotalAmount,
+  //   };
+  // }
 
   if (action.type === "REMOVE") {
     const existingCartItemIndex = state.items.findIndex(
